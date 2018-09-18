6eb09ced80d25e6a061c0a062dc587a517ae10f6
PJB3005 <pieterjan.briers@gmail.com>
2018-09-18 10:15:40 +0200

    Add --license and --copyright args.

--- scripts/RSI.py
+++ scripts/RSI.py
@@ -3,6 +3,7 @@
 import argparse
 from pathlib import Path
 from rsi import Rsi
+from typing import Optional
 
 
 def main() -> None:
@@ -12,18 +13,24 @@ def main() -> None:
     _from_dmi = subparser.add_parser("from_dmi", help="Will create an RSI from a BYOND DMI file.")
     _from_dmi.add_argument("input", help="The DMI file to read from.", type=Path)
     _from_dmi.add_argument("output", help="The RSI to output to.", type=Path)
+    _from_dmi.add_argument("--license", help="Specifies the license of the new RSI file.")
+    _from_dmi.add_argument("--copyright", help="Specifies the copyright of the new RSI file.")
 
     args = parser.parse_args()
 
     if args.command == "from_dmi":
-        from_dmi(args.input, args.output)
+        from_dmi(args.input, args.output, args.license, args.copyright)
         return
 
     print("No command specified!")
 
 
-def from_dmi(inputf: Path, output: Path) -> None:
+def from_dmi(inputf: Path, output: Path, new_license: Optional[str], new_copyright: Optional[str]) -> None:
     rsi = Rsi.from_dmi(inputf)
+    if new_license:
+        rsi.license = new_license
+    if new_copyright:
+        rsi.copyright = new_copyright
     rsi.write(output)
 
 
