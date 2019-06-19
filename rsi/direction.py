c2d274000561d8b24e403a01acd312b5c3b2dda9
ZNixian <minermoder27@gmail.com>
2019-06-20 00:49:47 +1200

    Add support for diagonal directions (#1)

--- rsi/direction.py
+++ rsi/direction.py
@@ -7,14 +7,32 @@ class Direction(Enum):
     EAST = 2
     WEST = 3
 
+    SOUTH_EAST = 4
+    SOUTH_WEST = 5
+    NORTH_EAST = 6
+    NORTH_WEST = 7
+
     def to_byond(self) -> int:
+        BY_NORTH = 1
+        BY_SOUTH = 2
+        BY_EAST = 4
+        BY_WEST = 8
+
         if self == Direction.NORTH:
-            return 1
+            return BY_NORTH
         elif self == Direction.SOUTH:
-            return 2
+            return BY_SOUTH
         elif self == Direction.EAST:
-            return 4
+            return BY_EAST
         elif self == Direction.WEST:
-            return 8
+            return BY_WEST
+        elif self == Direction.SOUTH_EAST:
+            return BY_SOUTH | BY_EAST
+        elif self == Direction.SOUTH_WEST:
+            return BY_SOUTH | BY_WEST
+        elif self == Direction.NORTH_EAST:
+            return BY_NORTH | BY_EAST
+        elif self == Direction.NORTH_WEST:
+            return BY_NORTH | BY_WEST
         else:
             raise ValueError()
