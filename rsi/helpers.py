cb811190cbbf2af2114b2d4ae80b4d4466a99196
Quantomicus <quantumgleam@gmail.com>
2020-02-04 23:43:57 +0100

    Remove selector support (#2)

--- rsi/helpers.py
+++
@@ -1,8 +0,0 @@
-from typing import List, Optional
-
-
-def state_name(name: str, selectors: Optional[List[str]] = None) -> str:
-    if selectors:
-        name += "+" + "+".join(sorted(selectors))
-
-    return name
